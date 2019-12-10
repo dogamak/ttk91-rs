378f200df3347e064d381448d3cd6c9d2ebecb7d
Mitja Karhusaari <mitja@karhusaari.me>
2019-12-10 02:40:39 +0200

    ttk91run: Parse command-line arguments with Clap

--- bin/run.rs
+++ bin/run.rs
@@ -4,14 +4,14 @@ use ttk91::{
     error::ParseError,
     emulator::{Emulator, Memory, StdIo},
 };
-
 use std::env::args;
+
+use clap::{App, Arg, ArgMatches};
 
 enum Error {
     Parse,
     Execution,
     IO(std::io::Error),
-    Argument,
 }
 
 impl From<std::io::Error> for Error {
@@ -26,34 +26,28 @@ impl<K> From<ParseError<K>> for Error {
     }
 }
 
-fn print_usage() {
-    println!("ttk91run: Compile and run TTK91 programs");
-    println!("");
-    println!("Usage: ttk91run [--help] <program>");
-    println!("");
-    println!(" --help, -h  Display this help text.");
-    println!("  <program>  The .b91 or .k91 program file.");
+fn parse_arguments() -> ArgMatches<'static> {
+    App::new("ttk91run")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Mitja Karhusaari <mitja@karhusaari.me>")
+        .about("Utility for compiling and executing TTK91 programs")
+        .arg(Arg::with_name("source")
+             .help("File containing assemby source or bytecode")
+             .value_name("SOURCE")
+             .required(true)
+             .index(1))
+        .get_matches()
 }
 
 fn main() {
-    let args: Vec<_> = args().collect();
+    let args = parse_arguments();
 
-    if args.len() == 1 ||
-        args.iter().find(|arg| *arg == "--help").is_some() ||
-        args.iter().find(|arg| *arg == "-h").is_some()
-    {
-        print_usage();
-        return;
-    }
-
-    let file_path = args.iter().skip(1).find(|arg| *arg != "--help" && *arg != "-h")
-        .unwrap();
+    let file_path = args.value_of("source").unwrap();
 
     match run(file_path) {
         Ok(()) => (),
         Err(Error::IO(io)) => eprintln!("IO error: {}", io),
         Err(Error::Execution) => eprintln!("Execution error"),
-        Err(Error::Argument) => print_usage(),
         Err(Error::Parse) => eprintln!("Parse error"),
     }
 }
