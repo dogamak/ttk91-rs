7e2c5d2cb7142f9202207381f15c16b751ef0dd8
Mitja Karhusaari <mitja@karhusaari.me>
2019-12-11 00:27:21 +0200

    parser: Make first operand optional, default to R0

--- src/symbolic/parser.rs
+++ src/symbolic/parser.rs
@@ -284,18 +284,22 @@ fn take_concrete_instruction(input: &str) -> Result<ConcreteInstruction> {
         tuple((
             take_concrete_opcode,
             sp,
-            operand1,
-            opt(sp),
-            tag(","),
-            opt(sp),
+            opt(terminated(
+                operand1,
+                tuple((
+                    opt(sp),
+                    tag(","),
+                    opt(sp),
+                )),
+            )),
             operand2,
         )),
         |tuple| {
             ConcreteInstruction {
                 label: None,
                 opcode: tuple.0,
-                operand1: tuple.2,
-                operand2: tuple.6,
+                operand1: tuple.2.unwrap_or(Register::R0),
+                operand2: tuple.3,
             }
         }
     )(input)
